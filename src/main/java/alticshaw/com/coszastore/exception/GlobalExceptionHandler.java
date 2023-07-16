21f2a736b6628000d081d1160c9d6f37060a2687
VietTam29 <nguyenviettam2905@gmail.com>
2023-07-16 17:55:52 +0700

    other commit

--- src/main/java/alticshaw/com/coszastore/exception/GlobalExceptionHandler.java
+++ src/main/java/alticshaw/com/coszastore/exception/GlobalExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.security.auth.message.AuthException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
