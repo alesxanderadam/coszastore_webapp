b13bacccce585859212b128ff36329b99f4998a8
mradam2301 <mradam.th2301@gmail.com>
2023-07-30 19:13:34 +0700

    complete upload image multipart

--- src/main/java/alticshaw/com/coszastore/controller/ProductController.java
+++ src/main/java/alticshaw/com/coszastore/controller/ProductController.java
@@ -1,81 +1,99 @@
 package alticshaw.com.coszastore.controller;
 
 import alticshaw.com.coszastore.payload.request.ProductRequest;
-import alticshaw.com.coszastore.payload.response.BaseResponse;
-import alticshaw.com.coszastore.payload.response.MessageResponse;
-import alticshaw.com.coszastore.payload.response.ProductResponse;
+import alticshaw.com.coszastore.payload.response.*;
+import alticshaw.com.coszastore.payload.response.ApiResponse;
 import alticshaw.com.coszastore.service.imp.ProductServiceImp;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
+@Api(tags = "Product Controller", description = "APIs related to products")
 public class ProductController {
-    private final BaseResponse baseResponse;
     private final MessageResponse messageResponse;
     private final ProductServiceImp productServiceImp;
 
     @Autowired
     public ProductController(
-            BaseResponse baseResponse,
             MessageResponse messageResponse,
             ProductServiceImp productServiceImp
     ) {
-        this.baseResponse = baseResponse;
         this.messageResponse = messageResponse;
         this.productServiceImp = productServiceImp;
     }
 
     @GetMapping
-    public ResponseEntity<?> getAll() {
+    @ApiOperation(value = "Get all products", notes = "Retrieve a list of all products")
+    public ResponseEntity<ApiResponse<List<ProductResponse>>> getAll() {
         List<ProductResponse> productResponses = productServiceImp.getAll();
-        baseResponse.setMessage(messageResponse.success());
-        baseResponse.setStatusCode(200);
-        baseResponse.setData(productResponses);
-        return ResponseEntity.ok().body(baseResponse);
+        ApiResponse<List<ProductResponse>> response = new ApiResponse<>();
+        response.setStatusCode(200);
+        response.setMessage("Success");
+        response.setData(productResponses);
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/{product_id}")
-    public ResponseEntity<?> getById(@PathVariable("product_id") Integer product_id) {
+    @ApiOperation(value = "Get a product by ID", notes = "Retrieve a product by its unique ID")
+    public ResponseEntity<ApiResponse<ProductResponse>> getById(
+            @ApiParam(value = "Product ID", required = true)
+            @PathVariable("product_id") Integer product_id) {
         ProductResponse productResponses = productServiceImp.getProductById(product_id);
-        baseResponse.setMessage(messageResponse.success());
-        baseResponse.setStatusCode(200);
-        baseResponse.setData(productResponses);
-        return ResponseEntity.ok().body(baseResponse);
+        ApiResponse<ProductResponse> response = new ApiResponse<>();
+        response.setMessage(messageResponse.success());
+        response.setStatusCode(200);
+        response.setData(productResponses);
+        return ResponseEntity.ok().body(response);
     }
 
-    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<?> add(@ModelAttribute @RequestBody @Valid ProductRequest productRequest, BindingResult bindingResult) {
+    @PostMapping()
+    @ApiOperation(value = "Create a new product", notes = "Requires product information")
+    public ResponseEntity<ApiResponse<ProductResponse>> add(
+            @ApiParam(name = "Product add" ,value = "Product information", required = true) @RequestBody @Valid ProductRequest productRequest,
+            BindingResult bindingResult
+    ) {
         ProductResponse product = productServiceImp.addProduct(productRequest, bindingResult);
-
-        baseResponse.setMessage(messageResponse.success());
-        baseResponse.setStatusCode(200);
-        baseResponse.setData(product);
-        return ResponseEntity.ok().body(baseResponse);
+        ApiResponse<ProductResponse> response = new ApiResponse<>();
+        response.setMessage(messageResponse.success());
+        response.setStatusCode(200);
+        response.setData(product);
+        return ResponseEntity.ok().body(response);
     }
 
-    @PutMapping(value = "/{product_id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<?> add(@ModelAttribute @RequestBody @Valid ProductRequest productRequest, @PathVariable("product_id") Integer product_id, BindingResult bindingResult) {
+    @PutMapping(value = "/{product_id}")
+    @ApiOperation(value = "Update an existing product", notes = "Update an existing product based on its ID")
+    public ResponseEntity<ApiResponse<ProductResponse>> update(
+            @ApiParam(value = "Product informat1ion", required = true) @ModelAttribute @RequestBody @Valid ProductRequest productRequest,
+            @ApiParam(value = "Product ID", required = true) @PathVariable("product_id") Integer product_id,
+            BindingResult bindingResult
+    ) {
         ProductResponse product = productServiceImp.updateProduct(productRequest, product_id, bindingResult);
+        ApiResponse<ProductResponse> response = new ApiResponse<>();
 
-        baseResponse.setMessage(messageResponse.success());
-        baseResponse.setStatusCode(200);
-        baseResponse.setData(product);
-        return ResponseEntity.ok().body(baseResponse);
+        response.setMessage(messageResponse.success());
+        response.setStatusCode(200);
+        response.setData(product);
+        return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{product_id}")
-    public ResponseEntity<?> delete(@PathVariable("product_id") Integer product_id) {
+    @ApiOperation(value = "Delete a product", notes = "Delete a product by its ID")
+    public ResponseEntity<ApiResponse<String>> delete(
+            @ApiParam(value = "Product ID", required = true) @PathVariable("product_id") Integer product_id) {
         productServiceImp.deleteProduct(product_id);
+        ApiResponse<String> response = new ApiResponse<>();
 
-        baseResponse.setMessage(messageResponse.success());
-        baseResponse.setStatusCode(200);
-        return ResponseEntity.ok().body(baseResponse);
+        response.setMessage(messageResponse.success());
+        response.setStatusCode(200);
+        return ResponseEntity.ok().body(response);
     }
 }
